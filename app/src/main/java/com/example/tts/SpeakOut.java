23087b8dd86d3a324977c1526a5c80e3f5f5322d
dltmdcks98 <dltmdcks98@gmail.com>
2022-01-18 13:37:55 +0900

    보안상의 이유로 직접적인 설정 변겅 X  => 접근성 메뉴로 이동 필요시 UI 선택창에서 TTS로 상태 출력 UI 제작시 FOCUS 설정 주의깊게 봐야할 듯
    추가적으로 tts출력 방법 명시
    talkback 사용 예제 : https://gamjatwigim.tistory.com/200?category=671262

--- app/src/main/java/com/example/tts/SpeakOut.java
+++ app/src/main/java/com/example/tts/SpeakOut.java
@@ -1,28 +1,42 @@
 package com.example.tts;
 
-import android.app.Activity;
+import static android.speech.tts.TextToSpeech.ERROR;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.speech.tts.TextToSpeech;
-import android.speech.tts.TextToSpeech.OnInitListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SpeakOut extends AppCompatActivity {
-    Button btnspeakout;
-    EditText edtTexttoSpeak;
+import java.util.Locale;
 
+public class SpeakOut extends AppCompatActivity {
+    Button btnspeakout,btnsetting;
+    EditText edtTexttoSpeak;
+    TextToSpeech tts; //TTS 변수 선언
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.speakout);
         btnspeakout = (Button) findViewById(R.id.btnspeakout);
+        btnsetting = (Button) findViewById(R.id.setting);
         edtTexttoSpeak = (EditText) findViewById(R.id.edtTexttoSpeak);
-        btnspeakout.setOnClickListener(new View.OnClickListener() {
+
+        //TTS 생성
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != ERROR) {
+                    // 언어를 선택한다.
+                    tts.setLanguage(Locale.ENGLISH);
+                }
+            }
+        });
+        btnsetting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //접근성 메뉴로 이동
@@ -30,5 +44,24 @@ public class SpeakOut extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        btnspeakout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tts.setPitch(2.0f);         // 음성 톤을 2.0배 올려준다.
+                tts.setSpeechRate(1.0f);    // 읽는 속도는 기본 설정 2.0f = 2배 속도 빠르기 읽기
+                tts.speak(edtTexttoSpeak.getText().toString(),TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //TTS 객체 제거
+        if(tts != null) {
+            tts.stop();
+            tts.shutdown();
+            tts = null;
+        }
     }
 }
